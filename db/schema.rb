0bb452d749bf977e5c766d4f5effaa19b32dc185
George Eddie <geddie.code@gmail.com>
2018-03-26 17:44:16 +0100

    completed login system, working on logo and css for the navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326110601) do
+ActiveRecord::Schema.define(version: 20180326135903) do
+
+  create_table "streaks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
