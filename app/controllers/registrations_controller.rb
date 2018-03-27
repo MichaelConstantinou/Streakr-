7cc9210cfa4bb8032a58c00783e6edcc17767ce4
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-03-27 11:25:21 +0100

    created registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,5 @@
+class RegistrationsController < Devise::RegistrationsController
+  def after_sign_in_path_for(resource_or_scope)
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get '/', to: 'streaks#index'
 
+
 end
