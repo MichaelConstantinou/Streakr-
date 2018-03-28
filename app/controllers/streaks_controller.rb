b304542b5bd685ec5ca6fd9c5983d100d7bf7750
Kerri Marusiak <33982042+Kerrimaru@users.noreply.github.com>
2018-03-28 18:37:08 +0100

    WIP update streak frequency

--- app/controllers/streaks_controller.rb
+++ app/controllers/streaks_controller.rb
@@ -2,7 +2,7 @@ class StreaksController < ApplicationController
   before_action :authenticate_user!
   def index
     @new_streak = Streak.new
-    if current_user.streaks.length>0
+    if current_user.streaks.length > 0
       render 'streaks/index'
     else
       render 'streaks/welcome'
@@ -10,8 +10,15 @@ class StreaksController < ApplicationController
   end
 
   def create
-    @new_streak = Streak.new(user_id: current_user.id, aim:params[:streak][:aim], title: params[:streak][:title])
+    @new_streak = Streak.new(frequency: 0, user_id: current_user.id, aim: params[:streak][:aim], title: params[:streak][:title])
     @new_streak.save
+    render 'streaks/index'
+  end
+
+  def update 
+    #binding.pry
+    #@streak = Streak.find(params[:id])
+    #@streak.update_attributes(params[:frequency])
     render 'streaks/index'
   end
 
